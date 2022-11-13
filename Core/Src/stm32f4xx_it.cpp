66747cd31036735889ddeeece3093f78249866ee
Bocha91 <bocha91@gmail.com>
2022-11-13 15:15:51 +0300

    Теперь FreeRTOS использует TIM10 как системныый

--- Core/Src/stm32f4xx_it.cpp
+++ Core/Src/stm32f4xx_it.cpp
@@ -60,7 +60,7 @@ extern DMA_HandleTypeDef hdma_adc1;
 extern ADC_HandleTypeDef hadc1;
 extern TIM_HandleTypeDef htim3;
 extern UART_HandleTypeDef huart1;
-extern TIM_HandleTypeDef htim1;
+extern TIM_HandleTypeDef htim10;
 
 /* USER CODE BEGIN EV */
 
@@ -221,7 +221,7 @@ void TIM1_UP_TIM10_IRQHandler(void)
     /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 0 */
 
     /* USER CODE END TIM1_UP_TIM10_IRQn 0 */
-    HAL_TIM_IRQHandler(&htim1);
+    HAL_TIM_IRQHandler(&htim10);
     /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 1 */
 
     /* USER CODE END TIM1_UP_TIM10_IRQn 1 */
